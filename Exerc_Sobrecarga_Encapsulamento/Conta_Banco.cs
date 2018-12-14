dac6dc9e22cb2ff7996e7d5861ce585a32fab550
Vitor Perez <vitor.g.perez@hotmail.com>
2018-12-14 13:21:58 -0200

    Projeto ja terminado

---
+++ Exerc_Sobrecarga_Encapsulamento/Conta_Banco.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc_Sobrecarga_Encapsulamento
+{
+    class Conta_Banco
+    {
+        public int numeroConta { get; private set; }
+        public string nomeTitular { get; private set; }
+        public double saldo { get; private set; }
+
+        public Conta_Banco(int numeroConta, string nomeTitular)
+        {
+            this.numeroConta = numeroConta;
+            this.nomeTitular = nomeTitular;
+        }
+
+        public Conta_Banco (int numeroConta, string nomeTitular, double depositoInicial) : this(numeroConta, nomeTitular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public double Deposito(double deposito)
+        {
+            return saldo += deposito;
+        }
+
+        public double Saque(double saque)
+        {
+            return saldo -= (saque + 5);
+        }
+
+        public override string ToString()
+        {
+            return "Dados da Conta\nNumero da conta: "
+                + numeroConta
+                + ", Nome do Titular: "
+                + nomeTitular
+                + ", Saldo em conta: "
+                + saldo.ToString("F2");
+        }
+    }
+}
---
+++ Exerc_Sobrecarga_Encapsulamento/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc_Sobrecarga_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta_Banco novaConta;
+
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial(s / n) ?");
+            char condicao = char.Parse(Console.ReadLine());
+
+            if(condicao.ToString().ToUpper() == "S")
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double saldo = double.Parse(Console.ReadLine());
+                novaConta = new Conta_Banco(numeroConta, nomeTitular, saldo);
+            }
+            else
+            {
+                novaConta = new Conta_Banco(numeroConta, nomeTitular);
+            }
+
+            Console.WriteLine(novaConta);
+
+            Console.Write("\nEntre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine());
+            novaConta.Deposito(deposito);
+            Console.WriteLine(novaConta);
+
+            Console.Write("\nEntre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine());
+            novaConta.Saque(saque);
+            Console.WriteLine(novaConta);
+
+            Console.ReadLine();
+        }
+    }
+}
